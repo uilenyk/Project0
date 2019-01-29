0fcfac761c140c3e682cc4f55dddc81493232a28
Kyne Liu <uilenyk@gmail.com>
2019-01-29 13:33:55 -0500

    Finished main menu with limited testing

--- src/main/java/com/revature/ExecuteProgram.java
+++ src/main/java/com/revature/ExecuteProgram.java
@@ -2,7 +2,9 @@ package com.revature;
 
 public class ExecuteProgram {
 	public static void main(String[] args) {
-		WelcomeMenu m = new WelcomeMenu();
-		m.show();
+		//WelcomeMenu m = new WelcomeMenu();
+		//m.show();
+		MainMenu mm = new MainMenu();
+		mm.show();
 	}
 }
--- src/main/java/com/revature/MainMenu.java
+++ src/main/java/com/revature/MainMenu.java
@@ -1,19 +1,52 @@
 package com.revature;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import org.apache.log4j.Logger;
+
 public final class MainMenu implements Showable{
+	static Logger log = Logger.getRootLogger();
+	
 	public String show() {
-		System.out.println("Select from the menu below by entering the corrisponding number:");
-		System.out.println("0./tExit");
-		System.out.println("1./tAccounts");
-		System.out.println("2./tPersonal Information");
-		return "";
+		int choise = -1;
+		while(choise == -1) {
+			System.out.println("0.\tExit");
+			System.out.println("1.\tAccounts");
+			System.out.println("2.\tPersonal Information");
+			System.out.print("Select from the menu above by entering the corrisponding number: ");
+			choise = input();
+		}
+		log.info("input function returned "+choise);
+		switch (choise){
+		case 0:
+			return "exit";
+		case 1:
+			return "account";
+		case 2:
+			return "personal";
+		default:
+			log.error("In main menu, chosen option was not 0, 1, or 2 but got to case switch");
+			return "main";
+		}
 	}
 	
 	private int input() {
 		Scanner s = new Scanner(System.in);
-		int result = 0;
+		int result;
+		try{
+			result = s.nextInt();
+		} catch (InputMismatchException e) {
+			System.out.println("\nThat is not a valid selection.");
+			s.nextLine();
+			return -1;
+		}
+		if(result != 0 && result != 1 && result != 2) {
+			System.out.println("\nThat is not a valid selection.");
+			s.nextLine();
+			return -1;
+		}
+		s.nextLine();
 		return result;
 	}
 	
